c8088d13ba9a5aa6cd4641a21e023d9269c1db09
s1gm9 <113210463+Mathewsq369@users.noreply.github.com>
2024-01-05 17:39:24 +0300

    redirect to homepage works

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let username;
+let password;
 const mySubmit = document.getElementById("mySubmit");
-const myH1 = document.getElementById("myH1")
+const myH1 = document.getElementById("myH1");
+const homeH1 = document.getElementById("homeH1");
 
 mySubmit.onclick = function(){
-    username = document.getElementById("myInput").value;
-    myH1.textContent = `Welcome ${username}`;
+    username = document.getElementById("usernameInput").value;
+    password = document.getElementById("passwordInput").value;
+    window.location.href = "home.html";
+    homeH1.textContent = `Welcome ${username}`;
 }
