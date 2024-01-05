509a872342b9b8a42f0c792a84d8a051c7fc44e0
s1gm9 <113210463+Mathewsq369@users.noreply.github.com>
2024-01-06 00:26:32 +0300

    solved the navigation between pages step 1

--- home.js
+++ home.js
@@ -1,7 +1,12 @@
 let username = localStorage.getItem('username');
+let email = localStorage.getItem('email');
 const homeH1 = document.getElementById("homeH1");
+const homeUsername = document.getElementById("homeUsername");
+const homeEmail = document.getElementById("homeEmail");
 
 if (username)
 {
     homeH1.textContent = `Welcome ${username}`;
+    homeUsername.textContent = `Username: ${username}`;
+    homeEmail.textContent = `Your email adress is ${email}`;
 }
--- signUp.js
+++ signUp.js
@@ -1,40 +1,29 @@
-let count;
 let username;
 let email;
 let password;
 let confPassword;
 const mySubmit = document.getElementById("mySubmit");
-const myH1 = document.getElementById("myH1");
 
 mySubmit.onclick = function(){
-    count = 0;
+    event.preventDefault();
     username = document.getElementById("usernameInput").value;
     email = document.getElementById("emailInput").value;
     password = document.getElementById("newPasswordInput").value;
     confPassword = document.getElementById("confirmPasswordInput").value;
     
-    if(password == confPassword && password.length >= 4)
+    if(password == confPassword && (password.length >= 4 && confPassword >= 4))
     {
         password = confPassword;
         localStorage.setItem('username', username);
         localStorage.setItem('email', email);
-        window.location.href = "home.html";
+        window.location.replace("home.html");
     }
     else if(password == confPassword && password.length < 4)
     {
-        count += 2;
+        document.getElementById("error").textContent = "Passwords are too short (less than 4 characters)";
     }
     else
     {
-        count++;
+        document.getElementById("error").textContent = "Passwords do not match!!";
     }
-}
-
-if (count % 2 == 1)
-{
-    document.getElementById("error").textContent = "passwords do not match!!";
-}
-else if(count % 2 == 0)
-{
-    document.getElementById("error").textContent = "Passwords are too short (less than 4 characters)";
 }
