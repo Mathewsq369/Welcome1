58fbb39d6930390caf250ccd9f6f89f761787392
s1gm9 <113210463+Mathewsq369@users.noreply.github.com>
2024-01-05 22:56:07 +0300

    added login page and new css file for login page

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+let count;
 let username;
+let email;
 let password;
+let confPassword;
 const mySubmit = document.getElementById("mySubmit");
 const myH1 = document.getElementById("myH1");
 
 mySubmit.onclick = function(){
+    count = 0;
     username = document.getElementById("usernameInput").value;
-    localStorage.setItem('username', username);
-    password = document.getElementById("passwordInput").value;
-    window.location.href = "home.html";
+    email = document.getElementById("emailInput").value;
+    password = document.getElementById("newPasswordInput").value;
+    confPassword = document.getElementById("confirmPasswordInput").value;
+    
+    if(password == confPassword && password.length >= 4)
+    {
+        password = confPassword;
+        localStorage.setItem('username', username);
+        localStorage.setItem('email', email);
+        window.location.href = "home.html";
+    }
+    else if(password == confPassword && password.length < 4)
+    {
+        count += 2;
+    }
+    else
+    {
+        count++;
+    }
+}
+
+if (count % 2 == 1)
+{
+    document.getElementById("error").textContent = "passwords do not match!!";
+}
+else if(count % 2 == 0)
+{
+    document.getElementById("error").textContent = "Passwords are too short (less than 4 characters)";
 }
